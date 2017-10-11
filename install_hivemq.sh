c196bd82ea86a3e0e0dfae03f90bf92900bb6609
asicoe <martineden131@gmail.com>
2017-10-11 10:05:20 +0100

    Upgraded to latest hivemq graphite plugin version 3.1.1. See: https://www.hivemq.com/wp-content/uploads/hivemq-graphite-metrics-plugin-3.1.1.zip

--- install_hivemq.sh
+++ install_hivemq.sh
@@ -39,11 +39,15 @@ rm -rf $INSTALL_TEMP
 INSTALL_TEMP_GRAPHITE=/opt/graphite-plugin-install
 mkdir -p $INSTALL_TEMP_GRAPHITE
 cd $INSTALL_TEMP_GRAPHITE
-wget http://www.hivemq.com/wp-content/uploads/hivemq-graphite-metrics-plugin-3.0.0.zip
-unzip hivemq-graphite-metrics-plugin-3.0.0.zip
-rm hivemq-graphite-metrics-plugin-3.0.0.zip
+
+HIVEMQ_GRAPHITE_PLUGIN='hivemq-graphite-metrics-plugin-3.1.1'
+HIVEMQ_GRAPHITE_PLUGIN_ZIP_FILE_NAME=$HIVEMQ_GRAPHITE_PLUGIN'.zip'
+HIVEMQ_GRAPHITE_PLUGIN_JAR_FILE_NAME=$HIVEMQ_GRAPHITE_PLUGIN'.jar'
+wget https://www.hivemq.com/wp-content/uploads/$HIVEMQ_GRAPHITE_PLUGIN_ZIP_FILE_NAME
+unzip $HIVEMQ_GRAPHITE_PLUGIN_ZIP_FILE_NAME
+rm $HIVEMQ_GRAPHITE_PLUGIN_ZIP_FILE_NAME
 mkdir -p /opt/hivemq-modules/graphite-metrics-plugin
-mv hivemq-graphite-metrics-plugin-3.0.0.jar LICENSE.txt /opt/hivemq-modules/graphite-metrics-plugin
+mv $HIVEMQ_GRAPHITE_PLUGIN_JAR_FILE_NAME LICENSE.txt /opt/hivemq-modules/graphite-metrics-plugin
 mv graphite-plugin.properties /opt/hivemq-modules/graphite-metrics-plugin
 rm -rf $INSTALL_TEMP_GRAPHITE
 
